d519120069cd3235a0dd90c2540052bc4dde935d
omkarrajejadhav <150519984+omkarrajejadhav@users.noreply.github.com>
2024-03-16 23:55:54 +0530

    Update Q12.cpp

--- Q12.cpp
+++ Q12.cpp
@@ -1,39 +1,39 @@
-//Q.12 Write a program to print the name, salary and date of joining of 10 employees in a company. Use array of objects.
+/*
+12
+Write a program to print the name, salary and date of joining of 10 employees in a company.
+ Use array of objects.
+
+Practice Problem
+*/
 
 #include<iostream>
 using namespace std;
 
-class company
-{
-private:
-float salary;
-string date;
-char name[50];
-public:
-    void set()
-    {
-        cout<<"\n Enter employee= \n name salary and date ";
-        cin>>name>>salary>>date;
-    }
-    void get()
-    {
-        cout<<"\nName: "<<name<<"\nSalary: "<<salary<<"\nDate of joining: "<<date;
-    }
-};
-main()
-{
-    company c1;
-    int i,count ,size=10;
-    cout<<"\n10 Employee details";
-    for ( i = 0; i < size; i++)
-    {
-        c1.set();
-        count++;
-    }
-    for ( i = 0; i <size; i++)
-    {
-      c1.get();
-      count++;
-    }
+class Employee{
+    private:
+        char name[30];
+        int year;
+        float salary;
+        public:
+            void set(){
+                cout<<"enter the employee name ,year of joining and salary";
+                cin>>name>>year>>salary;
+            }
+
+            void get(){
+                cout<<"\n"<<name<<"\t"<<year<<"\t"<<salary;
+            }
     
+};
+
+main(){
+    Employee e1[10];
+    cout<<"\n enter the  information of 10 employee \n";
+    for(int i=0;i<10;i++){
+        e1[i].set();
+    }
+    cout<<"\n Name"<<"\t year"<<"\t salary";
+    for(int i=0;i<10;i++){
+        e1[i].get();
+    }
 }
